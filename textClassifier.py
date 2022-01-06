ec30a593f2e837ffd67ae7f8cf4f90acab505a52
Javi Alonso <javier.alonso@live.u-tad.com>
2022-01-07 00:24:17 +0100

    Text classifier model works

--- textClassifier.py
+++ textClassifier.py
@@ -8,15 +8,50 @@ data = keras.datasets.imdb
 
 word_index = data.get_word_index()
 
-word_index = {k: (v+3) for k, v in word_index.items()}
+word_index = {k: (v + 3) for k, v in word_index.items()}
 word_index["<PAD>"] = 0
 word_index["<START>"] = 1
 word_index["<UNK>"] = 2
 word_index["<UNUSED>"] = 3
 
-reverse_word_index = dict([(value, key) for (key, value) in word_index.item()])
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post",
+                                                        maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-print(decode_review(data[0]))
+
+# model down here
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+model.summary()
+
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fit_model = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=2)
+
+results = model.evaluate(test_data, test_labels)
+print(results)
+
+test_review = test_data[0]
+print(test_review)
+predict = model.predict([tf.expand_dims(test_review, axis=0)])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: " + str(predict[0]))
+print("Actual: " + str(test_labels[0]))
