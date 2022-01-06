70b9e292cba5e7a039445c218b27a9af7c64134b
Javi Alonso <javier.alonso@live.u-tad.com>
2022-01-06 14:15:41 +0100

    Fix image classifier output

--- learning.py
+++ learning.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 data = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
-print(train_labels[6])
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt',
                'Sneaker', 'Bag', 'Ankle boot']
@@ -27,11 +26,11 @@ model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=
 # train model
 model.fit(train_images, train_labels, epochs=5)
 
-prediction = model.predict([test_images[7]])
+prediction = model.predict(test_images)
 
 for x in range(5):
     plt.grid(False)
     plt.imshow(test_images[x], cmap=plt.cm.binary)
-    plt.xlabel("Actual: " + test_labels[x])
-    plt.title("Prediction " + class_names[np.argmax(prediction[x])])
+    plt.xlabel('Actual: ' + class_names[test_labels[x]])
+    plt.title('Prediction ' + class_names[np.argmax(prediction[x])])
     plt.show()
